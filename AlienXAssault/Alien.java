f63d29d34a060373e9be35c6894b918522e66616
Lac-Phong Nguyen <Nguyen00902>
2019-09-12 09:43:42 -0700

    Base Alien Files

---
+++ AlienXAssault/Alien.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Alien extends Actor
+{
+    int SPEED = -7; // Speed of 10 in left direction "-"
+    public Alien() {
+    }
+
+    public void act() 
+    {
+        move (SPEED); //calls SPEED function
+
+        Actor Bullet = getOneIntersectingObject(Bullet.class); 
+        
+        // If alien touches left side of screen - Game Over
+         if (getX()<0) {
+             Greenfoot.playSound("game_over.mp3");
+            GameOver gameover = new GameOver();
+            getWorld().addObject(gameover, getWorld().getWidth()/2, getWorld().getHeight()/2);
+            Greenfoot.stop();
+        }
+        // If Bullet touches alien - Game Over
+        if (Bullet !=null) {
+            Greenfoot.playSound("explosion.wav");
+            getWorld(). removeObject(Bullet);
+
+            ((SpaceLand)(getWorld())).score.add(50);
+
+            getWorld(). removeObject(this); //remove alien from screen 
+        }
+        
+       
+    }
+}
---
+++ AlienXAssault/SpaceLand.java
@@ -0,0 +1,32 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class SpaceLand extends World
+{
+    Score score;
+    public SpaceLand()
+    {    
+        super(900, 600, 1, false); 
+
+        addObject (new Soldier(), 50, 300);
+        
+        score = new Score();
+        addObject(score, 500, 25);
+
+    }
+    public void act ()
+    {
+        int Alien = Greenfoot.getRandomNumber(101);
+        if (Alien >99) {
+            addObject(new Alien(), 1000, 300);
+        }
+        int Alien2 = Greenfoot.getRandomNumber(101);
+        if (Alien2 >99) {
+            addObject(new Alien(), 1000, 100);
+        }
+        int Alien3 = Greenfoot.getRandomNumber(101);
+        if (Alien3 >99) {
+            addObject(new Alien(), 1000, 500);
+        }
+    }
+}
+
