b69a488e52c75bdb5bd66d2adb02fb942f8b008a
daylenkb <daylenkboen@gmail.com>
2019-09-19 21:45:10 -0700

    implemented lacs arrays of strings, probably version to submit

--- Alien.java
+++ Alien.java
@@ -7,8 +7,13 @@ public class Alien extends Actor
     int SPEED = -7; // Speed of 10 in left direction "-"
     int tempType = (int)(Math.random() * ((2-0) + 1));
     int controlType = 0;
-    int moduleThree = 0;
     long lastAdded = System.currentTimeMillis();
+    String[] nouns = {"fortnite", "shield", "school", "tree", "mouse"};
+    GreenfootImage noun = new GreenfootImage(nouns[(int)(Math.random() * 4)], 30, Color.BLACK, Color.WHITE);
+    String[] verbs = {"run","walk","crawl","suck","build"};
+    GreenfootImage verb = new GreenfootImage(verbs[(int)(Math.random() * 4)], 30, Color.BLACK, Color.WHITE);
+    String[] adj = {"stupid","small","big","goofy","special"};
+    GreenfootImage adject = new GreenfootImage(adj[(int)(Math.random() * 4)], 30, Color.BLACK, Color.WHITE);
     
     public Alien() {
     
@@ -16,22 +21,29 @@ public class Alien extends Actor
     
     public void act() {
        move (SPEED);
+       var moduleThree = 0;
+       int change = 0;
        long curTime = System.currentTimeMillis();
-       if (curTime >= lastAdded + 5000) //5000ms = 5s
+       if (curTime > lastAdded + 2000)//5000ms = 5s
        {
-           controlType++;
+           controlType = controlType + 1;
            moduleThree = controlType % 3;
+           change = moduleThree;
+       }
+
+       if(change == 0){
+               ((SpaceLand)(getWorld())).pos.swap("noun");
+           }
+       else if(change  == 1){
+               ((SpaceLand)(getWorld())).pos.swap("verb");
+           }
+       else if(change == 2){
+               ((SpaceLand)(getWorld())).pos.swap("adjective");   
+       }
+       else {
+        
        }
        
-       if(moduleThree == 0){
-               ((SpaceLand)(getWorld())).pos.swap("bee");
-           }
-       else if(moduleThree  == 1){
-               ((SpaceLand)(getWorld())).pos.swap("alien");
-           }
-       else if(moduleThree  == 2){
-               ((SpaceLand)(getWorld())).pos.swap("soldier");   
-       }
        /**
        for (int i = 0; i<=10;i = i+1) 
        {
@@ -55,28 +67,27 @@ public class Alien extends Actor
        Actor Bullet = getOneIntersectingObject(Bullet.class); 
 
        if (tempType == 2) {
-           setImage("Soldier.png");
+           setImage(adject);
        }
        else if (tempType == 1){
-           setImage("Alien.png");
+           setImage(verb);
        }
        else {
-           setImage("bee.png");
+           setImage(noun);
        }
        
 
 
        // If alien touches left side of screen - Game Over
        if (getX()<0) {
-          if (tempType == moduleThree){
-              /**
+          if (tempType == 0){
            Greenfoot.playSound("game_over.mp3");
            GameOver gameover = new GameOver();
            getWorld().addObject(gameover, getWorld().getWidth()/2, getWorld().getHeight()/2);
            Greenfoot.stop();
-           **/
           }
-          else {}
+          else {
+          }
        }
         // If Bullet touches alien - Game Over
        if (Bullet !=null) {
